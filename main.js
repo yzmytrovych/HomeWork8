5ae4b56e42c6c65192ab0ba250bd396c619d953f
Julia Zmytrovych <97659424+YuliyaZmytrovych@users.noreply.github.com>
2024-08-12 17:59:48 +0300

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log('Hey, this is my JS program'); 
+
+
+function capitalizeStrings(words)
+{
+    let result = [];
+   for(let i=0; i<words.length;i++)
+   {
+       let temp = words[i].charAt(0).toUpperCase() ;
+       result.push(temp + words[i].toLowerCase().slice(1));
+   }
+   return  result;
+}
+
+const words = ["apple", "banaNA", "kiWi", "ORANGE"];
+console.log(capitalizeStrings(words)); // ["Apple", "Banana", "Kiwi",
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
+
+function capitalizeStrings(array1, array2)
+{
+    let result = [];
+    for (let i = 0; i < array1.length; i++) {
+        for (let j = 0; j < array2.length; j++) {
+            if (array1[i] === array2[j]) 
+                result.push(array1[i]);
+        }
+    }
+    return  result;
+}
+console.log(findCommonElements(array1, array2)); // [3, 4, 5]
+
+
+
+
